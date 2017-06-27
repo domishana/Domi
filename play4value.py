8ae3894bfa2bffbf88e2fe1883243a40cef6a817
rareshana <shana3698shana@gmail.com>
2017-06-27 18:25:26 +0900

    参加人数と自動試合回数を変数で指定するようにした

--- play4value.py
+++ play4value.py
@@ -1,19 +1,20 @@
 #coding: utf-8
 import play
 
-game = play.game_setup(2,1)
+playernum = 2
+times_of_battle = 10
 
-player_winpts = [0 for i in range(game.number)]
+player_winpts = [0 for i in range(playernum)]
 
-for m in range(10): #対戦回数
-    game = play.game_setup(2,1)
+for m in range(times_of_battle):
+    game = play.game_setup(playernum,1)
     game.begingame()
 
     nowmax = 0     #最高点
     playnum = [i for i in range(game.number)]
     scores = [game.player[i].victorycount() for i in range(game.number)]
 
-    for (i,n) in zip(playnum,scores):
+    for (i, n) in zip(playnum, scores):#勝利判定は多分本来はgameクラスに書かないといけなさそう(そのうちちゃんと書きます！！！)
         if nowmax <= n:
             nowmax = n
             winnerIndex = i
